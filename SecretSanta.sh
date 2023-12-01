7d485b858336a6c63593cf4c8de1903801964723
Stephanie Wang <7941538+evastgh@users.noreply.github.com>
2023-12-01 14:43:11 -0800

    Secret Santa assignments!

--- SecretSanta.sh
+++ SecretSanta.sh
@@ -67,7 +67,9 @@ mkdir assignments
 for i in {1..$N}; do
     echo "$names[i] is the Secret Santa for $shuffled_names[i]." >> .HIDE_assignments.txt
     echo "You are the Secret Santa for $shuffled_names[i]." > assignments/${hashes[i]}.txt
+    echo "$names[i] should take a look at https://raw.githubusercontent.com/evastgh/SecretSanta/master/assignments/${hashes[i]}.txt" > HIDE_encrypted_assignments.txt
 done
 git add assignments/*.txt
-
-# rm TEMP_*
+git commit -am "Secret Santa assignments!"
+git push
+rm TEMP_*
